bed404d3a410bd5f9360348b4025b944aae133f0
Agility6 <agility1013@gmail.com>
2025-01-28 17:11:17 +0800

    chore: init theme

---
+++ src/.config/default.ts
@@ -0,0 +1,93 @@
+import type { ThemeConfig } from '~/types'
+
+// This is the default configuration for the template, please do not modify it directly.
+// You can override this configuration in the `.config/user.ts` file.
+
+export const defaultConfig: ThemeConfig = {
+  site: {
+    title: 'Agility6',
+    subtitle: 'Blog',
+    author: 'Agility6',
+    description: 'Rediscory the beauty of typography',
+    website: 'https://astro-theme-typography.vercel.app/',
+    pageSize: 5,
+    socialLinks: [
+      {
+        name: 'github',
+        href: 'https://github.com/Agility6',
+      },
+      {
+        name: 'twitter',
+        href: 'https://x.com/Tang86d',
+      },
+      // {
+      //   name:'email',
+      //   href:'mailto:',
+      // },
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      },
+    ],
+    navLinks: [
+      {
+        name: 'Posts',
+        href: '/',
+      },
+      {
+        name: 'Archive',
+        href: '/archive',
+      },
+      {
+        name: 'Categories',
+        href: '/categories',
+      },
+      {
+        name: 'About Me',
+        href: '/about',
+      },
+    ],
+    categoryMap: [{ name: '胡适', path: 'hu-shi' }],
+    footer: [
+      '© %year <a target="_blank" href="%website">%author</a>',
+      'Theme <a target="_blank" href="https://github.com/Moeyua/astro-theme-typography">Typography</a> by <a target="_blank" href="https://moeyua.com">Moeyua</a>',
+      'Proudly published with <a target="_blank" href="https://astro.build/">Astro</a>',
+      `Email: agility1013@gmail.com`
+    ],
+  },
+  appearance: {
+    theme: 'system',
+    locale: 'en-us',
+    colorsLight: {
+      primary: '#2e405b',
+      background: '#ffffff',
+    },
+    colorsDark: {
+      primary: '#FFFFFF',
+      background: '#232222',
+    },
+    fonts: {
+      header:
+        '"HiraMinProN-W6","Source Han Serif CN","Source Han Serif SC","Source Han Serif TC",serif',
+      ui: '"Source Sans Pro","Roboto","Helvetica","Helvetica Neue","Source Han Sans SC","Source Han Sans TC","PingFang SC","PingFang HK","PingFang TC",sans-serif',
+    },
+  },
+  seo: {
+    twitter: '@Tang86d',
+    meta: [],
+    link: [],
+  },
+  rss: {
+    fullText: true,
+  },
+  comment: {
+    // disqus: { shortname: "typography-astro" },
+  },
+  analytics: {
+    googleAnalyticsId: '',
+    umamiAnalyticsId: '',
+  },
+  latex: {
+    katex: false,
+  },
+}

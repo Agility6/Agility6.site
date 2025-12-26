7b0bc961df0aeec9dec3285d29cdf70b0d8e2350
Agility6 <agility1013@gmail.com>
2025-12-27 00:51:32 +0800

    feat: font change

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -4,4 +4,10 @@ export const userConfig: Partial<UserConfig> = {
   // Override the default config here
   // site: { title: "講評世界" },
   // seo: { twitter: "@moeyua13" },
+  appearance: {
+    fonts: {
+      header: '"Merienda","HiraMinProN-W6","Source Han Serif CN",serif',
+      ui: '"Merienda","Source Sans Pro","Roboto","Helvetica Neue",sans-serif',
+    },
+  },
 }
